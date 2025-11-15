029d852f67588d418fd0f93693a1ceccac19910c
dmrutherford <david@dmrutherford.com>
2025-11-15 14:03:20 +0000

    add feed generation

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,14 @@
 export const constants = {
     meta: {
-        title: 'Trope-Informed',
+        title: 'Trope-Informed', //TODO Should this be Trope_Informed?
         description: 'Default description goes here.',
+        mailingListUrl: '', //TODO what is this?
+        feed: {
+            title: 'Trope-Informed News',
+            description: 'Latest news from the Trope-Informed Team',
+            defaultAuthor: 'The Trope-Informed Team',
+            site: 'https://www.tropeinformed.com',
+            permalinkBase: 'news',
+        },
     },
 };
