e12cd833b6ac7bad09dff8d59630d2cf7e803cc1
dmrutherford <david@dmrutherford.com>
2025-11-20 11:25:27 +0000

    update assets path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.trope-informed.com',
+    site: 'https://www.tropeinformed.com',
     base: '/',
+    build: {
+        assets: 'assets',
+    },
     integrations: [sitemap()],
 });
